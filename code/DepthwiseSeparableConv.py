12f075834046efd0a1b92ed8ac68897b3bc24e74
jaejung <88542073+jaejungscene@users.noreply.github.com>
2022-11-12 15:46:00 +0900

    Create DepthwiseSeparableConv.py

---
+++ code/DepthwiseSeparableConv.py
@@ -0,0 +1,11 @@
+class DepthwiseSeparableConv(nn.Sequential):
+    def __init__(self, inch, outch, kernel, stride=1, padding=0):
+        super(DepthwiseSeparableConv,self).__init__(
+            nn.Conv2d(inch, inch, 3, stride, padding, groups=inch, bias=False),
+            nn.BatchNorm2d(),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(inch, outch, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(outch),
+            nn.ReLU(inplace=True),
+        )
